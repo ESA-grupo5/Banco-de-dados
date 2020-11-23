b921d7da7e98ea39730418024bfc22355a173634
Joana Darck <43012901+joanasoaresd@users.noreply.github.com>
2020-11-22 23:33:59 -0300

    create disciplina in database flashcards

--- database.sql
+++ database.sql
@@ -5,3 +5,12 @@ CREATE TABLE usuario
 	email TEXT,
 	password TEXT
 );
+
+CREATE table disciplina
+(
+	sub_matter_id INTEGER PRIMARY KEY,
+	nm_sub_matter TEXT,
+	description TEXT,
+	fk_user_id INTEGER,
+FOREIGN KEY(fk_user_id) REFERENCES usuario(user_id)
+);
