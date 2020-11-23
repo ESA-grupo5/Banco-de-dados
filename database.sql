232a7089ce11608c80a1d0a93cb4c880011d9ee7
IsmarRodgs-dce <51753232+IsmarRodgs-dce@users.noreply.github.com>
2020-11-23 05:37:53 +0300

    Update database.sql
    Creat table assunto

--- database.sql
+++ database.sql
@@ -14,3 +14,11 @@ CREATE table disciplina
 	fk_user_id INTEGER,
 FOREIGN KEY(fk_user_id) REFERENCES usuario(user_id)
 );
+CREATE TABLE assunto
+(
+	subject_id INTEGER PRIMARY KEY,
+	nm_subject TEXT,
+	description TEXT,
+	fk_sub_matter_id INTEGER, 
+FOREIGN KEY (fk_sub_matter_id) REFERENCES disciplina(sub_matter_id)
+)
