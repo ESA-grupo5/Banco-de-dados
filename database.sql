bc5fe2ffaa88e897d96ab46470514b5e38e2e88b
Clebson <43012757+clebsonf@users.noreply.github.com>
2020-11-27 02:21:16 -0300

    inserindo dados na tabela assunto

--- database.sql
+++ database.sql
@@ -49,5 +49,13 @@ FOREIGN key(teste_id) REFERENCES teste(teste_id),
 FOREIGN key(fk_card_id) REFERENCES cartao(card_id)
 );
 
+INSERT INTO assunto (nm_subject, description, fk_sub_matter_id)
+VALUES(“Relacionamentos ”,”Faça o relacionamento de suas entidades”,1);
+INSERT INTO assunto (nm_subject, description, fk_sub_matter_id)
+VALUES(“Tesde de usabilidade”,”Como fazer testes de usabilidade”,2);
+INSERT INTO assunto (nm_subject, description, fk_sub_matter_id)
+VALUES(“Arquivos”,”Como os arquivos são gerenciados”,3);
+INSERT INTO assunto (nm_subject, description, fk_sub_matter_id)
+VALUES(“Classes”,””,4);
 
 
